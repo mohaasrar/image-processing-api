5de3f4f5fcb2a4d53e326fd695173d554cf8d343
melkam <melkamuhun@gmail.com>
2023-01-05 22:17:42 +0300

    fix:images api error message

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -6,15 +6,15 @@ import fs from 'fs';
 
 const images = express.Router();
 
-images.get('/', async (req: express.Request, res: express.Response) => {
+images.get('/', async (req: express.Request, res: express.Response): Promise<void | string> => {
   try {
     const fname = req.query.fname as unknown as string;
     const h = parseInt(req.query.height as unknown as string);
     const w = parseInt(req.query.width as unknown as string);
 
-    if (fname === undefined || fname === undefined || fname === undefined) {
+    if (fname === undefined || h === undefined || w === undefined) {
       res.status(400).send('please enter all parameters');
-      return;
+      
     }
 
     const ishnum = Number(h);
@@ -27,6 +27,13 @@ images.get('/', async (req: express.Request, res: express.Response) => {
       res.status(400).send('width must be number');
       return;
     }
+    const fileExists  = path.resolve(__dirname, `../../../assets/images/full/${fname}.jpg`);
+
+    if (!fs.existsSync(fileExists)) {
+      res.status(400).send('file name does not exist');
+      return;
+    }
+  
     const imagePath: string = `public/images/${fname}${h}x${w}.jpg`;
 
     if (!fs.existsSync(imagePath)) {
@@ -36,6 +43,7 @@ images.get('/', async (req: express.Request, res: express.Response) => {
     res.sendFile(path.resolve(imagePath));
   } catch (err) {
     res.send('An error occurred please check the file name and parameters');
+    return;
   }
 });
 
